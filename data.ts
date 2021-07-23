fe1634b8860fdba431cb73af5214fbef3634456f
kwin64 <kwin11bvb@yandex.by>
2021-07-23 15:16:52 +0300

    utils

--- data.ts
+++ data.ts
@@ -1,30 +1,18 @@
 const fs = require("fs");
+const { readJsonFromFile, writeJsonToFile } = require("./fs-utils.ts");
 
 let getUsers = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile("users.json", function (err, buf) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(buf.toString()));
-      }
-    });
-  });
+  return readJsonFromFile("users.json");
 };
 
 let addUser = async (name) => {
   let users = await getUsers();
   users.push({ id: 89, name: name });
-
-  return new Promise((resolve, reject) => {
-    fs.writeFile("users.json", JSON.stringify(users), (err) => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+  writeJsonToFile("users.json", users);
 };
 
-let deleteUser = (id) => {
+let deleteUser = async (id) => {
+  let users = await getUsers();
   users.filter((u) => u.name !== id);
 };
 
